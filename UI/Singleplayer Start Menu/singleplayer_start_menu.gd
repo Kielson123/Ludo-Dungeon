8307ec5d24acafdcccc965a7623412b128d572b8
Gracek12 <gracjanprzymus12@gmail.com>
2024-10-07 07:54:55 +0200

    Singleplayer menu

--- UI/Singleplayer Start Menu/singleplayer_start_menu.gd
+++ UI/Singleplayer Start Menu/singleplayer_start_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
+@onready var bot_count_input: LineEdit = $"MarginContainer/VBoxContainer/PanelContainer/MarginContainer/VSplitContainer/HSplitContainer/GameOptionsContainer/Bot Count Option/LineEdit"
 
+var max_player_count: int = 4
 
 func _on_exit_button_pressed() -> void:
 	Signals.request_main_menu.emit()
+
+
+func _on_line_edit_text_changed(new_text: String) -> void:
+	var count: int = clamp(int(bot_count_input.text), 0, max_player_count - 1)
+	bot_count_input.text = str(count)
