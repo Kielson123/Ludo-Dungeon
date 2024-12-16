0e5f050c3329a5685e24613f5a35fc313b7702c6
Gracek12 <gracjanprzymus12@gmail.com>
2024-12-16 07:46:47 +0100

    Slight changes to movement

--- Game/Players/player.gd
+++ Game/Players/player.gd
@@ -28,17 +28,17 @@ func _process(_delta: float) -> void:
 	
 	previous_menu_opened = menu_opened
 
-func move(tiles: Array[TextureRect], amount: int) -> void:
+func get_destination_tile(tiles: Array[TextureRect], distance_from_current: int) -> TextureRect:
 	var current_tile: TextureRect = get_parent()
 	var destination_tile: TextureRect = current_tile
 	for i in range(tiles.size()):
-		var tile_number: int = i + amount
+		var tile_number: int = i + distance_from_current
 		if tile_number > 39:
 			tile_number = (tile_number % 39) - 1
 		if tiles[i] == current_tile:
 			destination_tile = tiles[tile_number]
 			break
-	if destination_tile == current_tile:
-		printerr("Cannot move to the specified tile!")
-		return
+	return destination_tile
+
+func move(destination_tile: TextureRect) -> void:
 	reparent(destination_tile, false)
