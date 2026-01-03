b2e48cd32c0be8d3703ecd3a5959294cab3df864
Kielson <gracjanprzymus12@gmail.com>
2026-01-03 19:58:05 +0100

    Reset

--- Game/game.gd
+++ Game/game.gd
@@ -1,16 +1,1 @@
 extends Control
-
-@onready var board: Board = $"Board-4"
-@onready var actors: Array[Actor]
-
-func _ready() -> void:
-	for child in $Players.get_children():
-		if child is Actor:
-			actors.append(child)
-	
-	TurnManager.turns = actors
-	
-	for tile in board.tiles:
-		for actor in actors:
-			if actor is Player:
-				tile.tile_pressed.connect(actor._on_tile_pressed)
