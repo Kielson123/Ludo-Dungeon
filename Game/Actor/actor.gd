0db92a87bec78950e5f4b47babb7bd93f967c2b1
Kielson <gracjanprzymus12@gmail.com>
2026-01-26 22:41:12 +0100

    Added pawn textures

--- Game/Actor/actor.gd
+++ Game/Actor/actor.gd
@@ -1,14 +1,20 @@
 class_name Actor extends Node
 
-@export var color: Global.Colors
+@export var color: Global.Colors = Global.Colors.NONE
 @export var pawn_path: Array[int]
 @export var pawns: Array[Pawn]
 
 var selected_pawn: Pawn
+
+func _process(_delta: float) -> void:
+	if not pawns.is_empty() and pawns[0].color == Global.Colors.NONE:
+		for pawn in pawns:
+			pawn.set_color(color)
 
 func _input(event: InputEvent) -> void:
 	selected_pawn = pawns[0]
 	if event.is_action_pressed(&"ui_down"):
 		if selected_pawn.path_index + 1 >= 52:
 			Global.game_won.emit(self)
+			return
 		selected_pawn.path_index += 1
--- Game/Pawn/pawn.gd
+++ Game/Pawn/pawn.gd
@@ -2,10 +2,14 @@ class_name Pawn extends Node2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-@export var pawn_texture: Texture2D = preload("res://Assets/Game/Pawns/default.svg")
+@export var color: Global.Colors = Global.Colors.NONE
 
 @export var in_spawn: bool = true
 @export var path_index: int
 
 func _ready() -> void:
-	sprite_2d.texture = pawn_texture
+	sprite_2d.self_modulate = Global.get_rgb_from_color(color)
+
+func set_color(new_color: Global.Colors) -> void:
+	color = new_color
+	sprite_2d.self_modulate = Global.get_rgb_from_color(new_color)
